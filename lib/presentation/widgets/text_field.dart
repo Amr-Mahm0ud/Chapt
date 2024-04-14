8697647e41cc414dfc3423a375e1f44db5c626bb
Amr-Mahm0ud <amrosy00@gmail.com>
2024-04-14 21:07:56 +0200

    Changing Input field into widget to has its own context

--- lib/presentation/widgets/text_field.dart
+++ lib/presentation/widgets/text_field.dart
@@ -2,34 +2,64 @@ import 'package:flutter/material.dart';
 import '../resources/values_manager.dart';
 
 // ignore: non_constant_identifier_names
-TextFormField AppInputField({
-  required hint,
-  type = TextInputType.name,
-  required icon,
-  required controller,
-  validator,
-  bool obscure = false,
-  errorText,
-  Widget? action,
-}) {
-  return TextFormField(
-    keyboardType: type,
-    controller: controller,
-    obscureText: obscure,
-    validator: validator,
-    decoration: InputDecoration(
-      prefixIcon: Icon(icon),
-      suffixIcon: action,
-      filled: true,
-      hintText: hint,
-      errorText: errorText,
-      contentPadding: const EdgeInsets.all(AppValues.v12),
-      border: OutlineInputBorder(
-        borderSide: BorderSide.none,
-        borderRadius: BorderRadius.circular(
-          AppValues.v10,
-        ),
+class AppInputField extends StatefulWidget {
+  final String? hint;
+  final TextInputType type;
+  final IconData? icon;
+  final TextEditingController? controller;
+  final dynamic validator;
+  final bool obscure;
+  final String? errorText;
+  final bool haveErrorText;
+  final Widget? action;
+  final bool filled;
+  final InputBorder? border;
+  const AppInputField({
+    super.key,
+    this.hint,
+    this.type = TextInputType.name,
+    this.icon,
+    this.controller,
+    this.validator,
+    this.obscure = false,
+    this.errorText = '',
+    this.haveErrorText = true,
+    this.action,
+    this.filled = true,
+    this.border,
+  });
+
+  @override
+  State<AppInputField> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<AppInputField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: widget.type,
+      controller: widget.controller,
+      obscureText: widget.obscure,
+      validator: widget.validator,
+      style: Theme.of(context).textTheme.bodyMedium,
+      decoration: InputDecoration(
+        prefixIcon: widget.icon == null ? null : Icon(widget.icon),
+        suffixIcon: widget.action,
+        filled: widget.filled,
+        hintText: widget.hint,
+        errorText: widget.haveErrorText ? widget.errorText : null,
+        contentPadding: const EdgeInsets.symmetric(
+            vertical: AppValues.v12, horizontal: AppPadding.p20),
+        enabledBorder: widget.border,
+        disabledBorder: widget.border,
+        border: widget.border ??
+            OutlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.circular(
+                AppValues.v10,
+              ),
+            ),
       ),
-    ),
-  );
+    );
+  }
 }
